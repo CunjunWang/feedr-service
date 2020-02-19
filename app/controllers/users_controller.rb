4ca0d982047eaef158c09d13f92f4b61b49470fb
Jaye <jiefanli97@gmail.com>
2020-02-19 18:25:54 -0500

    Added edit user function and restructured user module

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
 
   # skip_before_action :authorized, only: [:new, :create]
+  def show
+    @user = User.find(session[:user_id])
+  end
 
   def new
     if session[:user_id] and !User.find(session[:user_id]).nil?
@@ -10,7 +13,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params.require(:user).permit(:email, :password, :first_name, :last_name, :phone))
+    @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
       redirect_to '/welcome'
@@ -19,7 +22,22 @@ class UsersController < ApplicationController
     end
   end
 
-  def me
+  def edit
     @user = User.find(session[:user_id])
+  end
+
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :password, :first_name, :last_name, :phone)
   end
 end
