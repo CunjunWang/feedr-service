12c2ab21372974e334512354987240af1b6e0904
Jaye <jiefanli97@gmail.com>
2020-02-18 22:46:41 -0500

    added user model and login, sign up, and authentication modules

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+
+  skip_before_action :authorized, only: [:new, :create]
+
+  def new
+    if session[:user_id] and !User.find(session[:user_id]).nil?
+      return redirect_to '/welcome'
+    end
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params.require(:user).permit(:email, :password, :first_name, :last_name, :phone))
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to '/welcome'
+    else
+      render 'new'
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  resources :users, only: [:new, :create]
+  get '/', to: 'sessions#welcome'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  get 'welcome', to: 'sessions#welcome'
+  get 'authorized', to: 'sessions#page_requires_login'
+  get 'logout', to: 'sessions#logout'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
