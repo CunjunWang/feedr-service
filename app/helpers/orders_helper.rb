4bed39ea2bed97ff89e2b28bc6ba4823c82fab2e
Cunjun Wang <duckwcj@gmail.com>
2020-02-22 14:31:09 -0500

    add place order function

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,3 +1,5 @@
+require 'securerandom'
+
 module OrdersHelper
   def transform_order_status(order_status_code)
     if order_status_code == 1
@@ -12,4 +14,16 @@ module OrdersHelper
       'CANCELED'
     end
   end
+
+  def calculate_item_subtotal(price, quantity)
+    res = price * quantity
+    @order_subtotal += res
+    res
+  end
+
+  def self.generate_order_no
+    order_no = SecureRandom.uuid
+    order_no.gsub('-', '')
+  end
+
 end
