236bcc91eee062b566e2803d64196dd5867ffe52
Cunjun Wang <duckwcj@gmail.com>
2020-02-22 11:12:47 -0500

    add item info on order page

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,12 +1,27 @@
 class OrdersController < ApplicationController
 
   def my
-    @orders = Order.where('user_id = 1')
+    # check login status
+    # unless logged_in?
+    #   redirect_to '/login'
+    #   return
+    # end
+    # user_id = session[:user_id]
+    user_id = 2
+    @orders = Order.where("user_id = #{user_id}")
     logger.info "Find #{@orders.length} orders"
 
+    @items = {}
     @orders.each do |o|
-      logger.info "order subtotal: #{o.order_subtotal}"
+      order_no = o.order_no
+      item_arr = OrderItem.where("order_no = '#{order_no}'")
+      logger.info("item length = #{item_arr.length}")
+      @items[order_no] = item_arr
     end
+  end
+
+  def detail
+
   end
 
 end
