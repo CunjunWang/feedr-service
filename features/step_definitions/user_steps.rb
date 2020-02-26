8717dba652bea065b0b50103ebd274895c585ea0
Jaye <jiefanli97@gmail.com>
2020-02-26 18:57:35 -0500

    Added behavior test for user login, sign up and search modules

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -7,3 +7,17 @@ Given(/^there is an existing user with email "([^"]*)" and password "([^"]*)"$/)
   @user = User.new({email: email, password: password, first_name: 'Jason', last_name: 'Lee', phone: '6463870372'})
   @user.save
 end
+
+Given(/^There are following orders$/) do |order_table|
+  # table is a table.hashes.keys # => [:order_no, :truck_id, :user_id, :order_status, :order_subtotal, :is_del, :truck_name]
+  order_table.hashes.each_with_index do |order, index|
+    Order.new(order).save
+  end
+end
+
+Given(/^There are following users$/) do |user_table|
+  # table is a table.hashes.keys # => [:email, :password, :first_name, :last_name, :phone]
+  user_table.hashes.each_with_index do |user, index|
+    User.new(user).save
+  end
+end
